0a8652ccc653c24896727906c419b93890bc2161
pavel <kulikovps2004@gmail.com>
2024-03-20 23:54:41 +0300

    upadted Server url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = "https://russian-agrarian-group.onrender.com/"
+export const SERVER_URL = "https://russian-agrarian-group-backend.onrender.com/"
 // export const SERVER_URL = "http://localhost:5000/"
